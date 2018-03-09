933400cc7ad1f203ec6d1c1824654d2ce3e502f6
Jasper <jaspercheng.is@gmail.com>
2018-03-08 19:08:43 -0800

    Auth for music app

---
+++ MusicApp/app/models/user.rb
@@ -0,0 +1,61 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+
+  validates :password, length: {allow_nil: true, minimum: 4}
+  validates :email, uniqueness: true
+  validates :session_token, uniqueness: true
+
+  attr_reader :password
+
+  after_initialize :set_session_token
+  # after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom.base64(16)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+
+    if user && user.is_password?(password)
+      return user
+    end
+  end
+
+  def set_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_session_token
+    self.save!
+
+    self.session_token
+  end
+
+  def ensure_session_token(session_token)
+    self.session_token == session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(@password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest) == password
+  end
+
+
+end
